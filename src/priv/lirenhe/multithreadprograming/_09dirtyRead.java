69874d5e2cbf2447936a863727d40f595f0953cd
Li Renhe <mark-lirenhe@qq.com>
2017-12-07 20:23:29 +0800

    add something

---
+++ src/priv/lirenhe/multithreadprograming/_09dirtyRead.java
@@ -0,0 +1,8 @@
+package priv.lirenhe.multithreadprograming;
+
+
+//脏读的实现：setValue和getValue
+//脏读：不同线程争取value的情况（seter和getter的方法）
+public class _09dirtyRead {
+
+}
---
+++ src/priv/lirenhe/multithreadprograming/_910chongruLock.java
@@ -0,0 +1,8 @@
+package priv.lirenhe.multithreadprograming;
+
+//一个线程调用一个对象，将锁这个对象，但是他仍然可以重新调用该对象的其他synchronized方法。
+//                                 调用该对象父类中的其他synchronized方法。
+
+public class _910chongruLock {
+
+}
